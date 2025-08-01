c84ef026b41f19903b64f284e5eae6065f4ff9a1
Danillo Victtor <danilo_victor2004@hotmail.com>
2025-08-01 08:33:07 -0300

    fix: ajuste no menu fixo no topo no responsivo.

--- js/main.js
+++ js/main.js
@@ -1,21 +1,38 @@
 const btnMenu = document.getElementById('btn-menu');
 const nav = document.querySelector('aside nav');
 const body = document.body;
+const main = document.querySelector('main');
 
 btnMenu.addEventListener('click', () => {
   const isOpen = nav.classList.toggle('open');
   btnMenu.setAttribute('aria-expanded', isOpen);
-
   body.classList.toggle('no-scroll', isOpen);
 });
 
-// Fecha o menu ao clicar em um link
 nav.querySelectorAll('a').forEach(link => {
-  link.addEventListener('click', () => {
+  link.addEventListener('click', (e) => {
     if (nav.classList.contains('open')) {
+      e.preventDefault();
+
+      const targetId = link.getAttribute('href');
+
+      // Fecha o menu
       nav.classList.remove('open');
       btnMenu.setAttribute('aria-expanded', false);
       body.classList.remove('no-scroll');
+
+      setTimeout(() => {
+        if (targetId.startsWith('#')) {
+          const targetElement = main.querySelector(targetId);
+          if (targetElement) {
+            const offsetTop = targetElement.offsetTop;
+            main.scrollTo({
+              top: offsetTop,
+              behavior: 'smooth'
+            });
+          }
+        }
+      }, 100);
     }
   });
 });
